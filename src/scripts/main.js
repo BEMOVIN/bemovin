703d9860f12bb3b6af0d90a4f1fd21b4390bba59
Nikolay Georgiev <master033@gmail.com>
2018-12-02 03:44:42 +0200

    Add animations on scroll.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,14 @@ import {
 } from './utils';
 import 'bootstrap';
 import $ from 'jquery';
+import AOS from 'aos';
 
 greet();
+
+AOS.init({
+  duration: 500,
+  once: true
+});
 
 $(document).ready(function () {
   $("a").on('click', function (event) {
