3ff9976f182af720235bf6784f4664e8d70ce781
Nikolay Georgiev <master033@gmail.com>
2018-12-02 03:19:14 +0200

    Add smooth scroll to sections through navigation menu.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,21 @@
-import { greet } from './utils';
+import {
+  greet
+} from './utils';
 import 'bootstrap';
+import $ from 'jquery';
 
 greet();
+
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
