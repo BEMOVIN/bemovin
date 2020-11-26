ef0f681b7f3e2d38590a0d45e6b7f48078f1821b
nyxz <master033@gmail.com>
2020-11-26 21:53:04 +0200

    Add the AOS animation library.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import { BootstrapVue } from 'bootstrap-vue'
+import AOS from 'aos'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'aos/dist/aos.css'
 
 Vue.config.productionTip = false
 
@@ -12,3 +14,5 @@ Vue.use(BootstrapVue)
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+AOS.init()
