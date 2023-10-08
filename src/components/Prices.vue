9eda24a949a80b984b3ffb4091bbd0cf7e65981e
Nikolay Georgiev <n.georgiev@bidpath.com>
2023-10-08 21:47:05 +0300

    feat: Update prices.

--- src/components/Prices.vue
+++ src/components/Prices.vue
@@ -2,11 +2,11 @@
   <div class="bmv-prices" id="prices">
     <span class="bmv-title">Цени</span>
     <span class="bmv-message"
-      >Карти за достъп до залата и групови тренировки</span
+      >Карти за Strength & skill and Conditioning classes</span
     >
     <div class="container">
       <div
-        class="row justify-content-between bmv-price-tags bmv-prices-facility"
+        class="row justify-content-between bmv-price-tags bmv-prices-strength-n-skill"
       >
         <div
           data-aos="fade-right"
@@ -17,7 +17,7 @@
             <div class="card-header">Карта 4</div>
             <div class="card-body">
               <h5 class="card-title">4 тренировки за месец</h5>
-              <h1 class="card-text bmv-price-label">45лв</h1>
+              <h1 class="card-text bmv-price-label">60лв</h1>
             </div>
           </div>
         </div>
@@ -30,7 +30,7 @@
             <div class="card-header">Карта 8</div>
             <div class="card-body">
               <h5 class="card-title">8 тренировки за месец</h5>
-              <h1 class="card-text bmv-price-label">80лв</h1>
+              <h1 class="card-text bmv-price-label">100лв</h1>
             </div>
           </div>
         </div>
@@ -43,7 +43,72 @@
             <div class="card-header">Карта 12</div>
             <div class="card-body">
               <h5 class="card-title">12 тренировки за месец</h5>
-              <h1 class="card-text bmv-price-label">100лв</h1>
+              <h1 class="card-text bmv-price-label">120лв</h1>
+            </div>
+          </div>
+        </div>
+        <div
+          data-aos="fade-right"
+          data-aos-delay="800"
+          class="col bmv-price-tag"
+        >
+          <div class="card accent">
+            <div class="card-body">
+              <h3>MEMBERSHIP ПРОГРАМА</h3>
+              <h5 class="card-title">Годишна такса</h5>
+              <div class="card-text">
+                <h1 class="bmv-price-label">1000лв</h1>
+                <p>Възможност за плащане на 3 вноски</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <span class="bmv-message"
+      >Карти за Handstand practice & Boby in motion</span
+    >
+    <div class="container">
+      <div
+        class="row justify-content-between bmv-price-tags bmv-prices-handstand-body-in-motion"
+      >
+        <div
+          data-aos="fade-right"
+          data-aos-delay="200"
+          class="col bmv-price-tag"
+        >
+          <div class="card">
+            <div class="card-header">Карта П4</div>
+            <div class="card-body">
+              <h5 class="card-title">4 тренировки за 45 дни</h5>
+              <h1 class="card-text bmv-price-label">85лв</h1>
+            </div>
+          </div>
+        </div>
+        <div
+          data-aos="fade-right"
+          data-aos-delay="400"
+          class="col bmv-price-tag"
+        >
+          <div class="card">
+            <div class="card-header">Карта П8</div>
+            <div class="card-body">
+              <h5 class="card-title">8 тренировки за 60 дни</h5>
+              <h1 class="card-text bmv-price-label">150лв</h1>
+            </div>
+          </div>
+        </div>
+        <div
+          data-aos="fade-right"
+          data-aos-delay="600"
+          class="col bmv-price-tag"
+        >
+          <div class="card">
+            <div class="card-header">Карта П12</div>
+            <div class="card-body">
+              <h5 class="card-title">12 тренировки за 90 дни</h5>
+              <h1 class="card-text bmv-price-label">220лв</h1>
             </div>
           </div>
         </div>
@@ -53,13 +118,10 @@
           class="col bmv-price-tag"
         >
           <div class="card">
-            <div class="card-header">Годишна</div>
+            <div class="card-header">Карт П16</div>
             <div class="card-body">
-              <h5 class="card-title">Годишен неограничен достъп</h5>
-              <div class="card-text">
-                <h1 class="bmv-price-label">800лв</h1>
-                <p>Възможност за плащане на 3 вноски</p>
-              </div>
+              <h5 class="card-title">16 тренировки за 120 дни</h5>
+              <h1 class="card-text bmv-price-label">275лв</h1>
             </div>
           </div>
         </div>
@@ -67,9 +129,6 @@
     </div>
 
     <span class="bmv-message">Абонаменти за треньор</span>
-    <span class="bmv-prices-subtitle-accent"
-      >*Абонаментите за треньор НЕ включват картите за достъп до залата</span
-    >
     <div class="container">
       <div
         class="row justify-content-between bmv-price-tags bmv-prices-trainers"
@@ -85,7 +144,7 @@
               <h5 class="card-title">
                 1 тренировка седмично в рамките на 1 месец
               </h5>
-              <h1 class="card-text bmv-price-label">105лв</h1>
+              <h1 class="card-text bmv-price-label">180лв</h1>
             </div>
           </div>
         </div>
@@ -100,7 +159,7 @@
               <h5 class="card-title">
                 2 тренировки седмично в рамките на 1 месец
               </h5>
-              <h1 class="card-text bmv-price-label">165лв</h1>
+              <h1 class="card-text bmv-price-label">295лв</h1>
             </div>
           </div>
         </div>
@@ -115,7 +174,7 @@
               <h5 class="card-title">
                 3 тренировки седмично в рамките на 1 месец
               </h5>
-              <h1 class="card-text bmv-price-label">220лв</h1>
+              <h1 class="card-text bmv-price-label">385лв</h1>
             </div>
           </div>
         </div>
@@ -156,31 +215,35 @@ export default {
     text-transform: uppercase;
   }
 
-  & .bmv-prices-subtitle-accent {
-    width: 100%;
-    display: block;
-    color: $c-palette-sec-primary;
-    text-transform: uppercase;
-    text-align: center;
-    margin: 1em 0;
-    font-size: 0.8em;
-    font-weight: 400;
-  }
-
   & .container {
     margin: 2em auto 4em auto;
   }
 
-  & .bmv-prices-facility {
+  & .bmv-prices-strength-n-skill {
     & .card {
       background-color: $c-palette-primary;
+      color: $text-light;
+      &.accent {
+        background: darken($c-palette-primary, 10%);
+        background: linear-gradient(
+          0deg,
+          darken($c-palette-primary, 10%) 0%,
+          $c-palette-primary 100%
+        );
+      }
+    }
+  }
+
+  & .bmv-prices-handstand-body-in-motion {
+    & .card {
+      background-color: $c-palette-sec-dark;
       color: $text-light;
     }
   }
 
   & .bmv-prices-trainers {
     & .card {
-      background-color: $c-palette-sec-dark;
+      background-color: $c-palette-accent-darker;
       color: $text-light;
     }
   }
